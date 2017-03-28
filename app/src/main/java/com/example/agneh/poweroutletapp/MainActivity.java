de3e93f620996c5bde0fae7c73b76b6ebe24bcf5
agneh <agne.hesten@gmail.com>
2017-03-28 11:44:52 +0200

    Simple application bar added (home button without functionality yet, test only)

--- app/src/main/java/com/example/agneh/poweroutletapp/MainActivity.java
+++ app/src/main/java/com/example/agneh/poweroutletapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.agneh.poweroutletapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Populates Menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //No other choices as for today. TO DO: remove switch if we do not come across other options
+        switch(item.getItemId()){
+            case R.id.Home:
+                Toast.makeText(this, "You clicked Home" , Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
 }
